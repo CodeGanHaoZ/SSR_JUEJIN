c83cf2a408ea53afcfaf7a4d780fe3eec0dc10ff
甘皓玮 <1436619890@qq.com>
2023-02-18 01:33:42 +0800

    header

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <Tutorial/>
+  <!-- <Header /> -->
+  <h1>1</h1>
 </template>
 
 <script>
+// import Header from '../components/home/header.vue'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  // components:{
+  //   Header
+  // }
 }
 </script>
