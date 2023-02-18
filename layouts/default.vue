cdd1411384cb25d667e56a5fc5d30cdb78060fee
甘皓玮 <1436619890@qq.com>
2023-02-18 20:31:56 +0800

    1

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from '../components/home/header.vue'
+export default {
+   components:{
+    Header
+  }
+}
+</script>
